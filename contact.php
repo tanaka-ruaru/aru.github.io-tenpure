d2652ee3c216cf8dc2f4cb9f57a1ff28190be01a
tanaka-ruaru <2p3jvg5o8@gmail.com>
2024-06-17 16:23:50 +0900

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,17 +5,6 @@
     <link rel="stylesheet" type="text/css" href="top.css">
     <title>お問い合わせありがとうございます</title>
 </head>
-    <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // POSTメソッドで送信された場合の処理
-    // ここにフォームデータの処理を記述する
-} else {
-    // POST以外のメソッドでアクセスされた場合の処理
-    // 通常はエラー処理を記述する
-    http_response_code(405);
-    echo "Method Not Allowed";
-    exit;
-}
 
 <div class="contact">
     <div class="thanks-message">お問い合わせいただきありがとうございます。<br>下記の内容で送信されました。</div>
@@ -26,4 +15,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div contact-list>内容</div>
     <?php echo $_POST["body"]; ?>
 </div>
-?>
+</html>
