1bcb4f0dbc466d31224154c59a50f314d3432ad3
tanaka-ruaru <2p3jvg5o8@gmail.com>
2024-06-17 13:26:47 +0900

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <div charset="utf-8">
-    <link rel="stylesheet" href="top.css">
+    <link rel="stylesheet" type="text/css" href="top.css">
 </div>
 <div class="contact">
     <div class="thanks-message">お問い合わせいただきありがとうございます。<br>下記の内容で送信されました。</div>
