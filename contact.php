6c2fc96b6daf6bd2aa282cb2aefd4df5599b166c
tanaka-ruaru <2p3jvg5o8@gmail.com>
2024-06-17 13:17:11 +0900

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<div charset="utf-8">
+    <link rel="stylesheet" href="top.css">
+</div>
+<div class="contact">
+    <div class="thanks-message">お問い合わせいただきありがとうございます。<br>下記の内容で送信されました。</div>
+    <div contact-list>名前</div>
+    <?php echo $_POST["name"]; ?>
+    <div contact-list>メールアドレス</div>
+    <?php echo $_POST["mail"]; ?>
+    <div contact-list>内容</div>
+    <?php echo $_POST["body"]; ?>
+</div>
